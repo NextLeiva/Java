c57bcf471478e41b9ec1558fa29cfe887f422a72
joelleiva <joel28leiva@gmail.com>
2022-08-06 02:33:46 -0500

    Hola mundo

---
+++ src/com/company/HolaMundo.java
@@ -0,0 +1,9 @@
+package com.company;
+//Mi clase de java
+public class HolaMundo {
+
+    //comentario
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Java");
+    }
+}
