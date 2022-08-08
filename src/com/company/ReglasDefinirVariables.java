fa0c642e3c8cd6a0ff697379a32883dff516745f
joelleiva <joel28leiva@gmail.com>
2022-08-07 21:30:04 -0500

    ReglasDefinirVariables

---
+++ src/com/company/ReglasDefinirVariables.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ReglasDefinirVariables {
+    public static void main(String[] args) {
+        // no caacteres especiales o numeros
+        var miVariable = 1;
+        var _miVariable = 2;
+        var $miVariable = 3;
+
+    }
+}
