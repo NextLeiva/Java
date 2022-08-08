ed7321feaea6ff5168b317e4abd02d96f20d92ab
joelleiva <joel28leiva@gmail.com>
2022-08-07 21:26:40 -0500

    InferenciaTipos con var y soutv

---
+++ src/com/company/InferenciaTipos.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class InferenciaTipos {
+    public static void main(String[] args) {
+        //var = Inferencia de tipos de java
+        int miVariableEntera = 10;
+        var miVariableEntera2 = 8;
+        System.out.println(miVariableEntera2);
+
+        var miVariableCadena ="Nataly";
+        System.out.println(miVariableCadena);
+        //Concatenacion de cadenas
+        System.out.println("miVariableCadena = " + miVariableCadena);
+    }
+}
+
+
