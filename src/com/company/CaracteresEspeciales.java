20683008c05832b6970e9d56a44caf01aeefd33c
joelleiva <joel28leiva@gmail.com>
2022-08-07 21:53:14 -0500

    CaracteresEspeciales

---
+++ src/com/company/CaracteresEspeciales.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class CaracteresEspeciales {
+    public static void main(String[] args) {
+        var nombre = "Nataly";
+        System.out.println("Nueva linea : \n " + nombre);
+        System.out.println("Tabulador :\t " + nombre);
+        System.out.println("Retroceso : \b " + nombre);
+        System.out.println("Retroceso: \b\b" + nombre);
+        System.out.println("Comilla simple: \'" + nombre + "\'");
+        System.out.println(" Comilla doble: \"" + nombre + "\"");
+    }
+}
