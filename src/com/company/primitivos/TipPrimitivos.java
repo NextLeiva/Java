6fe6a91e3b93dcd472683849534d1f1519b09595
joelleiva <joel28leiva@gmail.com>
2022-08-13 14:23:12 -0500

    tipo primitivo byte

---
+++ src/com/company/primitivos/TipPrimitivos.java
@@ -0,0 +1,13 @@
+package com.company.primitivos;
+
+public class TipPrimitivos {
+    public static void main(String[] args) {
+        /*
+        tipos primitivos enteros: byte,short,int long
+         */
+        byte numeroByte = (byte) 129;
+        System.out.println("Valor byte." + numeroByte);
+        System.out.println("valor minito byte" + Byte.MIN_VALUE);
+        System.out.println("valor minito byte" + Byte.MAX_VALUE);
+    }
+}
